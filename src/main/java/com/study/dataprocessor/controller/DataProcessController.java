7d989530c88675ddf1dce08e2d4898e4ea76ed45
Jonny-Cho <andante2183@gmail.com>
2022-03-13 01:50:46 +0900

    setting: swagger-ui 추가

--- src/main/java/com/study/dataprocessor/controller/DataProcessController.java
+++ src/main/java/com/study/dataprocessor/controller/DataProcessController.java
@@ -3,6 +3,8 @@ package com.study.dataprocessor.controller;
 import com.study.dataprocessor.dto.RequestProcess;
 import com.study.dataprocessor.dto.ResponseProcess;
 import com.study.dataprocessor.service.ProcessService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +18,7 @@ public class DataProcessController {
 
     private final ProcessService processService;
 
+    @Operation(summary = "data process", description = "URL 파싱후 데이터 가공")
     @PostMapping("process")
     public ResponseEntity<ResponseProcess> process(@RequestBody @Valid final RequestProcess request) {
         final ResponseProcess result = processService.parse(request);
