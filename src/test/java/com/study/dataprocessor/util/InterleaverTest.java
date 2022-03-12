be84814a07be87bcb73bf422796ea11c204e54d2
조준희/전시개발팀 <jonny.cho@musinsa.com>
2022-03-12 21:49:17 +0900

    feat: Interleaver 추가

---
+++ src/test/java/com/study/dataprocessor/util/InterleaverTest.java
@@ -0,0 +1,26 @@
+package com.study.dataprocessor.util;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class InterleaverTest {
+
+    @Autowired
+    private final Interleaver interleaver = new Interleaver();
+
+    @DisplayName("문자열 두개가 주어질 때 하나씩 교차하여 출력한다. 남은 것은 뒤에 붙인다")
+    @ParameterizedTest
+    @CsvSource({
+        "aaaaaaa, 1111, a1a1a1a1aaa",
+        "AAAAAbbbCCCCcDDdddEeee, 0123456789, A0A1A2A3A4b5b6b7C8C9CCcDDdddEeee",
+        "ABbCcD, 0123456789, A0B1b2C3c4D56789",
+    })
+    void test(final String str1, final String str2, final String result) {
+        assertThat(interleaver.interleave(str1, str2)).isEqualTo(result);
+    }
+
+}
