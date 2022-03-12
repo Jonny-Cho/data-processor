66205cc6580c6675a8453563fb13046641386f11
Jonny-Cho <andante2183@gmail.com>
2022-03-12 22:48:36 +0900

    feat: ProcessService 완성

--- src/main/java/com/study/dataprocessor/dto/ResponseProcess.java
+++ src/main/java/com/study/dataprocessor/dto/ResponseProcess.java
@@ -6,4 +6,9 @@ import lombok.Getter;
 public class ResponseProcess {
     private String quotient;
     private String remainder;
+
+    public ResponseProcess(final OutputUnit outputUnit) {
+        this.quotient = outputUnit.getQuotient();
+        this.remainder = outputUnit.getRemainder();
+    }
 }
