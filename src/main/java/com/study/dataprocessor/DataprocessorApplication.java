a67aabd1e53ac22ba5be95b1d132366ae55ad7ad
조준희/전시개발팀 <jonny.cho@musinsa.com>
2022-03-12 17:20:48 +0900

    setting: 프로젝트 세팅

---
+++ src/main/java/com/study/dataprocessor/DataprocessorApplication.java
@@ -0,0 +1,13 @@
+package com.study.dataprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataprocessorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataprocessorApplication.class, args);
+    }
+
+}
