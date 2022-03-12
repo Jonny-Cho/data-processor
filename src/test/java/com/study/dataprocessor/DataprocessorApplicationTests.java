fea3cdf8ffd4bb38b6a5f4e2190439a25e68f06f
Jonny-Cho <andante2183@gmail.com>
2022-03-12 17:20:48 +0900

    setting: 프로젝트 세팅

---
+++ src/test/java/com/study/dataprocessor/DataprocessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.dataprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataprocessorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
